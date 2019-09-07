7d87ea57d2f16d00d4f4b460b7ed5333f76e2d15
Elton MEdeiros Pereira <eltonmpereira85@outlook.com>
2019-09-07 08:45:18 -0300

    ClassePessoa

---
+++ classePessoa/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassePessoa
+{
+    class Pessoa
+    {
+        private String nome;
+        private int idade;
+        private String rg;
+
+        public Pessoa() { }
+
+        public Pessoa(String nome, String rg, int idade)
+        {
+            this.nome = nome;
+            this.rg = rg;
+            this.idade = idade;
+        }
+
+        public void Nome (String nome)
+        {
+            this.nome = nome;
+        }
+        public String Nome()
+        {
+            return this.nome;
+        }
+        public void Rg (String rg)
+        {
+            this.rg = rg;
+        }
+        public String Rg()
+        {
+            return this.rg;
+        }
+        public void Idade(int idade)
+        {
+            this.idade = idade;
+        }
+        public int Idade()
+        {
+            return this.idade;
+        }
+       
+    }
+        
+}
---
+++ classePessoa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome("Elton Medeiros");
+            p1.Idade(33);
+            p1.Rg("234.567-8");
+            
+
+            Console.WriteLine("nome.: " + p1.Nome());
+            Console.WriteLine("RG...: " + p1.Rg());
+            Console.WriteLine("Idade: " + p1.Idade());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Pessoa p2 = new Pessoa("Renata Maria", "3.323-43", 30);
+
+            Console.WriteLine("Nome.:" + p2.Nome());
+            Console.WriteLine("Rg...:" + p2.Rg());
+            Console.WriteLine("Idade:" + p2.Idade());
+
+            Console.ReadLine();
+        }
+    }
+}
